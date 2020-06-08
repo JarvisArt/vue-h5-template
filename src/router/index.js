0039714c08e9c29cf27cfdb06a0a5b355c88018f
Jarvis <1120886013@qq.com>
2020-06-08 23:00:06 +0800

    H5模板基础配置

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,7 @@ const routes = [
   {
     path: '/',
     name: 'Home',
+    meta: { title: '首页' },
     component: Home
   },
   {
@@ -24,6 +25,11 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title ? to.meta.title : 'vue-h5-template'
+  next()
 })
 
 export default router
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,28 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <div class="about">
+      <h1>This is an about page</h1>
+    </div>
+    <van-goods-action>
+      <van-goods-action-icon icon="chat-o" text="客服"  />
+      <van-goods-action-icon icon="cart-o" text="购物车"  />
+      <van-goods-action-icon icon="shop-o" text="店铺"  />
+      <van-goods-action-button
+        type="danger"
+        text="立即购买"
+      />
+    </van-goods-action>
   </div>
 </template>
+
+<script>
+import { GoodsAction, GoodsActionIcon, GoodsActionButton } from 'vant'
+export default {
+  name: 'About',
+  components: {
+    [GoodsAction.name]: GoodsAction,
+    [GoodsActionIcon.name]: GoodsActionIcon,
+    [GoodsActionButton.name]: GoodsActionButton
+  }
+}
+</script>
